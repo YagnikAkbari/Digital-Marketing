8f6d3bf7bd8b093944a6e3da30af92096ae0b772
Dhruvik91 <yashgondaliya04@gmail.com>
2024-03-23 21:34:21 +0530

    Add Contact Us Page

--- components/ContactUs.tsx
+++ components/ContactUs.tsx
@@ -1,7 +1,129 @@
 import React from "react";
+import styles from "../styles/ContactUs.module.scss";
 
-const ContactUs = () => {
-  return <div id="contact-us">ContactUs</div>;
-};
+export default function ContactPageOne() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-export default ContactUs;
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <div>
+      <div className="mx-auto max-w-7xl px-4">
+        <div className="mx-auto max-w-7xl py-12 md:py-24">
+          <div className="grid items-center justify-items-center gap-x-4 gap-y-10 lg:grid-cols-2">
+            {/* contact from */}
+            <div className="flex items-center justify-center">
+              <div className="px-2 md:px-12">
+                <p className="text-2xl font-bold text-gray-900 md:text-4xl">
+                  Get in touch
+                </p>
+                <p className="mt-4 text-lg text-gray-600">
+                  Our friendly team would love to hear from you.
+                </p>
+                <form action="" className="mt-8 space-y-4">
+                  <div className="grid w-full gap-y-4 md:gap-x-4 lg:grid-cols-2">
+                    <div className="grid w-full  items-center gap-1.5">
+                      <label
+                        className="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                        htmlFor="first_name"
+                      >
+                        First Name
+                      </label>
+                      <input
+                        className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                        type="text"
+                        id="first_name"
+                        placeholder="First Name"
+                      />
+                    </div>
+                    <div className="grid w-full  items-center gap-1.5">
+                      <label
+                        className="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                        htmlFor="last_name"
+                      >
+                        Last Name
+                      </label>
+                      <input
+                        className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                        type="text"
+                        id="last_name"
+                        placeholder="Last Name"
+                      />
+                    </div>
+                  </div>
+                  <div className="grid w-full  items-center gap-1.5">
+                    <label
+                      className="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                      htmlFor="email"
+                    >
+                      Email
+                    </label>
+                    <input
+                      className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                      type="text"
+                      id="email"
+                      placeholder="Email"
+                    />
+                  </div>
+                  <div className="grid w-full  items-center gap-1.5">
+                    <label
+                      className="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                      htmlFor="phone_number"
+                    >
+                      Phone number
+                    </label>
+                    <input
+                      className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                      type="tel"
+                      id="phone_number"
+                      placeholder="Phone number"
+                    />
+                  </div>
+                  <div className="grid w-full  items-center gap-1.5">
+                    <label
+                      className="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                      htmlFor="message"
+                    >
+                      Message
+                    </label>
+                    <textarea
+                      className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                      id="message"
+                      placeholder="Leave us a message"
+                      cols={3}
+                    />
+                  </div>
+                  <button
+                    type="button"
+                    className="w-full rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                  >
+                    Send Message
+                  </button>
+                </form>
+              </div>
+            </div>
+            <div className={styles["contact-text-right"]}>
+              <p>
+                We'd love to help you out with any queries or service-related
+                information at the earliest. Please drop in a line.{" "}
+              </p>
+              <h1 style={{ fontWeight: "bold", color: "white" }}>
+                {" "}
+                Working Hours
+              </h1>
+              <p>
+                10 AM to 6 PM IST <br />
+                Mon - Sat
+              </p>
+              <p style={{ color: "#B34400" }}>help@growwitup.com</p>
+              <p>Trusted by many creators.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr className="mt-6" />
+    </div>
+  );
+}
