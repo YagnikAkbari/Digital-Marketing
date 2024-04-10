afafcf9fcc85f4ff159eedc8461f4191993c5fad
YagnikAkbari2003 <yagnik.a@medkart.in>
2024-04-10 22:58:57 +0530

    add get a demo form

--- components/MainSection.tsx
+++ components/MainSection.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "../styles/MainSection.module.scss";
 import { useSelector } from "react-redux";
 import { RootState } from "@/store/store";
+import Link from "next/link";
 
 const MainSection = () => {
   const theme = useSelector((state: RootState) => state?.theme);
@@ -16,21 +17,23 @@ const MainSection = () => {
           Empower the world with <span>social</span>.
         </h1>
         <p className={styles["small"]}>
-          Do Different, Be Different<span></span>
+          Your Growth, Our Expertise<span></span>
         </p>
-        {/* <h1>
-          Helping the <span>world's</span> creators create.
-        </h1> */}
         <p>
           We are a dedicated team of professionals committed to assisting brands
           in achieving exponential growth through strategic utilization of
           social media platforms. Elevate your YouTube presence effortlessly
-          with our top-tier content creation services. As the trusted choice for
-          influential creators and ambitious entrepreneurs seeking to expand
-          their YouTube presence, we provide unparalleled strategy and
+          with our top-tier content creation services. As the trusted choice
+          fornp influential creators and ambitious entrepreneurs seeking to
+          expand their YouTube presence, we provide unparalleled strategy and
           production expertise.
         </p>
-        <button className={`call-to-action`}>Get Your Session</button>
+        <Link
+          href="https://forms.gle/Nc3fkUkcTmbbE4ff8"
+          className={`call-to-action`}
+        >
+          Get Your Session
+        </Link>
       </div>
       <img src="images/Hero.svg" alt="Hero..." />
     </div>
