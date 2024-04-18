9fb6390984e7f7db2d9ad57ba9ee244cd815b895
YagnikAkbari2003 <yagnik.a@medkart.in>
2024-04-18 17:57:02 +0530

    animation added

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,32 +1,48 @@
 import SectionHeader from "@/common/SectionHeader";
 import React from "react";
 import styles from "../styles/Clients.module.scss";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store/store";
 
 const Clients = () => {
+  const theme = useSelector((state: RootState) => state?.theme);
   return (
-    <div id="clients" className={`mt-32 ${styles.clients}`}>
+    <div
+      id="clients"
+      className={`mt-32 ${styles.clients} ${
+        theme === "m-light" ? styles["l-clients"] : ""
+      }`}
+    >
       <SectionHeader header="clients" prefix="our" />
       <div className="grid grid-cols-4 justify-between mt-20 mx-auto gap-4">
-        <img
-          src="images/Co-Buy.svg"
-          alt="brand 4..."
-          className={styles["brand-img"]}
-        />
-        <img
-          src="images/outperform.png"
-          alt="brand 3..."
-          className={styles["brand-img"]}
-        />
-        <img
-          src="images/OFk.jpg"
-          alt="brand 1..."
-          className={styles["brand-img"]}
-        />
-        <img
-          src="images/vicky.jpg"
-          alt="brand 2..."
-          className={styles["brand-img"]}
-        />
+        <div className={styles["brand-wrapper"]}>
+          <img
+            src="images/Co-Buy.svg"
+            alt="brand 4..."
+            className={styles["brand-img"]}
+          />
+        </div>
+        <div className={styles["brand-wrapper"]}>
+          <img
+            src="images/outperform.png"
+            alt="brand 3..."
+            className={styles["brand-img"]}
+          />
+        </div>
+        <div className={styles["brand-wrapper"]}>
+          <img
+            src="images/OFk.jpg"
+            alt="brand 1..."
+            className={styles["brand-img"]}
+          />
+        </div>
+        <div className={styles["brand-wrapper"]}>
+          <img
+            src="images/vicky.jpg"
+            alt="brand 2..."
+            className={styles["brand-img"]}
+          />
+        </div>
       </div>
     </div>
   );
--- components/MainSection.tsx
+++ components/MainSection.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/MainSection.module.scss";
 import { useSelector } from "react-redux";
 import { RootState } from "@/store/store";
@@ -6,6 +6,23 @@ import Link from "next/link";
 
 const MainSection = () => {
   const theme = useSelector((state: RootState) => state?.theme);
+  const [showContent, setShowContent] = useState<boolean>(true);
+
+  useEffect(() => {
+    let timer;
+    const toggleContent = () => {
+      setShowContent(!showContent);
+    };
+
+    if (showContent) {
+      timer = setTimeout(toggleContent, 5000);
+    } else {
+      timer = setTimeout(toggleContent, 1000);
+    }
+
+    return () => clearTimeout(timer); // Cleanup on component unmount
+  }, [showContent]);
+
   return (
     <div
       className={`flex items-center justify-between ${styles.hero} ${
@@ -17,16 +34,17 @@ const MainSection = () => {
           Empower the world with <span>social</span>.
         </h1>
         <p className={styles["small"]}>
-          Your Growth, Our Expertise<span></span>
+          Your Growth, Our Expertise
+          {showContent && <span></span>}
         </p>
         <p className={styles.para}>
           We are a dedicated team of professionals committed to assisting brands
           in achieving exponential growth through strategic utilization of
           social media platforms. Elevate your YouTube presence effortlessly
-          with our top-tier content creation services. As the trusted choice
-          fornp influential creators and ambitious entrepreneurs seeking to
-          expand their YouTube presence, we provide unparalleled strategy and
-          production expertise.
+          with our top-tier content creation services. As the trusted choice for
+          influential creators and ambitious entrepreneurs seeking to expand
+          their YouTube presence, we provide unparalleled strategy and
+          production expertise.
         </p>
         <Link
           href="https://forms.gle/Nc3fkUkcTmbbE4ff8"
