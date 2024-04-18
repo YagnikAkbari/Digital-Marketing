33b00773b931b5d27e156693adb335e57e393708
YagnikAkbari2003 <yagnik.a@medkart.in>
2024-04-18 21:28:51 +0530

    faq question changed

--- components/Faqs.tsx
+++ components/Faqs.tsx
@@ -7,27 +7,17 @@ import { RootState } from "@/store/store";
 
 const accrMenu = [
   {
-    question: " What is the turnaround time for videos?",
-    answer:
-      "We’re committed to providing the best results in the quickest time. This usually ranges between 2-4 days, depending on the length and type of videos.",
-  },
-  {
     question:
-      "Is your editing process standardized or adapted to each client's content?",
-    answer:
-      "We know every creator is different. Which is why we’re open to complete customisation according to your voice and requirements. Our team will even give you suggestions to improve your content.",
+      "What is the turnaround time for the video and what's packages are you offering?",
+    answer: "link",
   },
+
   {
     question: "What are the commercials?",
     answer:
       "Our pricing is flexible, depending on the duration and genre of the content. We offer monthly and per-project models.",
   },
-  {
-    question:
-      "Where is Growwitup Media located, and what are the working hours?",
-    answer:
-      "Our office is located in India, and working hours are 10 AM - 6 PM IST (Mon - Sat).",
-  },
+
   {
     question: "Do you create repurposed content for Reels / TikTok / Shorts?",
     answer:
@@ -37,6 +27,26 @@ const accrMenu = [
     question: "Do you offer packages for Reels / TikTok / Shorts?",
     answer:
       "Yes! The world is hooked to short form video and we know you want to get in on it. We offer monthly packages and deals for the same.",
+  },
+  {
+    question: "How does the onboarding process work?",
+    answer:
+      "Once you’re happy with the trial video, we mutually co-sign an agreement. You’ll get the trial video with the watermark removed, and we’ll have payment after each video for the first month.",
+  },
+  {
+    question: "How will I communicate with the Praper team?",
+    answer:
+      "We’ll be connected on a Slack channel, and you’ll be assigned a senior editor who will handle your projects. Our CEO will supervise all work, and you can connect with him if required.",
+  },
+  {
+    question: "What modes of payment do you accept?",
+    answer:
+      "We’re flexible, and accept a number of payment methods.\nInternational Payments: PayPal (5% extra for processing), Wise.com, Bill.com.\nIndian Payments: NEFT, UPI, or Net Banking.",
+  },
+  {
+    question: "How will we be exchanging files?",
+    answer:
+      "We’ll share a dropbox folder with you through which you can send and receive files without any loss in quality.",
   },
 ];
 
@@ -69,29 +79,41 @@ const Faqs = () => {
         >
           {accrMenu.map(
             (item: { question: string; answer: string }, idx: number) => (
-              <div key={idx} className={styles["single-accordion"]}>
-                <button
-                  onClick={() => toggleAccordion(idx)}
-                  className={styles["faq-closed"]}
-                >
-                  {item?.question}
-                  <span>
-                    <i
-                      className={`fas fa-sort-${
-                        activeTabs.includes(idx) ? "up" : "down"
-                      }`}
-                    ></i>
-                  </span>
-                </button>
-                <p
-                  className={`${
-                    activeTabs.includes(idx)
-                      ? styles["display"]
-                      : styles["hide"]
-                  }`}
-                >
-                  {item?.answer ?? ""}
-                </p>
+              <div key={idx}>
+                <div className={styles["single-accordion"]}>
+                  <button
+                    onClick={() => toggleAccordion(idx)}
+                    className={styles["faq-closed"]}
+                  >
+                    {item?.question}
+                    <span>
+                      <i
+                        className={`fas fa-sort-${
+                          activeTabs.includes(idx) ? "up" : "down"
+                        }`}
+                      ></i>
+                    </span>
+                  </button>
+                  <p
+                    className={`${
+                      activeTabs.includes(idx)
+                        ? styles["display"]
+                        : styles["hide"]
+                    }`}
+                  >
+                    {item?.answer === "link" ? (
+                      <a
+                        href="https://forms.gle/Nc3fkUkcTmbbE4ff8"
+                        className="link"
+                        target="_blank"
+                      >
+                        Get in touch
+                      </a>
+                    ) : (
+                      item?.answer
+                    )}
+                  </p>
+                </div>
               </div>
             )
           )}
